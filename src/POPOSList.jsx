bdf76587c4fd5e583bfc3618ffa4dc2910088b33
christianalves <christian.alves@students.dominican.edu>
2026-02-23 11:14:53 -0800

    Add dynamic POPOS data and styling

--- src/POPOSList.jsx
+++ src/POPOSList.jsx
@@ -1,40 +1,22 @@
 import POPOSSpace from './POPOSSpace.jsx'
+import './POPOSList.css'
+import data from './sfpopos-data.json'
 
 function POPOSList() {
-  return (
-    <div className="POPOSList">
+  const spaces = data.map(({ title, address, images, hours }) => {
+    return (
       <POPOSSpace
-        name="50 California Street"
-        address="50 California St."
-        image="50-california-st.jpg"
+        key={title}
+        name={title}
+        address={address}
+        image={images[0]}
+        hours={hours}
       />
-      <POPOSSpace
-        name="100 Pine Street"
-        address="100 Pine St."
-        image="100-pine-st.jpg"
-      />
-      <POPOSSpace
-        name="101 California Street"
-        address="101 California St."
-        image="101-california-st.jpg"
-      />
-      <POPOSSpace
-        name="343 Sansome Roof Garden"
-        address="343 Sansome St."
-        image="343-sansome-roof-garden.jpg"
-      />
-      <POPOSSpace
-        name="525 Market Street Plaza"
-        address="525 Market St."
-        image="525-market-street-plaza.jpg"
-      />
-      <POPOSSpace
-        name="Citigroup Center"
-        address="1 Sansome St."
-        image="citigroup-center-plaza.jpg"
-      />
-    </div>
-  )
+    )
+  })
+
+  return <div className="POPOSList">{spaces}</div>
 }
 
 export default POPOSList
+
--- src/POPOSSpace.jsx
+++ src/POPOSSpace.jsx
@@ -1,5 +1,7 @@
+import './POPOSSpace.css'
+
 function POPOSSpace(props) {
-  const { name, image, address } = props
+  const { name, image, address, hours } = props
 
   return (
     <div className="POPOSSpace">
@@ -11,6 +13,7 @@ function POPOSSpace(props) {
       />
       <h1>{name}</h1>
       <div>{address}</div>
+      {hours && <div className="POPOSSpace-hours">Hours: {hours}</div>}
     </div>
   )
 }
